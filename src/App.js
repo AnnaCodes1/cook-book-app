fa570d7b2402bacc1fcd4f9f9bf1f110a9f53185
AnnaCodes1 <annacodes1@gmail.com>
2022-11-04 20:47:24 +0000

    Provided new recipe with a number using index + cleared the input after adding recipe

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import MyButton from './components/button/MyButton'
 import MyInput from './components/input/MyInput'
 import RecipeList from './components/RecipeList'
 import './styles/App.css'
 
 function App() {
-  const [recipes, setRecipe] = useState([
+  const [recipes, setRecipes] = useState([
     { id: 1, title: 'Chicken soup', body: 'Hearty soup for cold days' },
     { id: 2, title: 'Pumpkin soup', body: 'Perfect for autumn chilly days' },
     { id: 3, title: 'Lentil soup', body: 'Spicy and rich soup for cold days' },
   ])
 
-  const [recipes2, setRecipe2] = useState([
+  const [recipes2, setRecipes2] = useState([
     { id: 1, title: 'Plov with meat', body: 'Hearty soup for cold days' },
     {
       id: 2,
@@ -26,16 +26,22 @@ function App() {
   ])
 
   const [title, setTitle] = useState('')
-  const bodyInputRef = useRef()
+  const [body, setBody] = useState('')
 
   const addNewRecipe = (e) => {
     e.preventDefault()
-    console.log(bodyInputRef.current.value)
+    const newRecipe = {
+      id: Date.now(),
+      title,
+      body,
+    }
+    setRecipes([...recipes, newRecipe])
+    setTitle('')
+    setBody('')
   }
 
   return (
     <div className="App">
-
       {/* {Управляемый компонент} */}
       <MyInput
         value={title}
@@ -44,8 +50,12 @@ function App() {
         placeholder="Title"
       />
 
-      {/* {Неуправляемый компонент} */}
-      <MyInput ref={bodyInputRef} type="text" placeholder="Description" />
+      <MyInput
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        type="text"
+        placeholder="Description"
+      />
 
       <MyButton onClick={addNewRecipe}>Add recipe</MyButton>
 
