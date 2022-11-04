759fc3eadad0780a29abdb1b189b9d1214b58e8c
AnnaCodes1 <annacodes1@gmail.com>
2022-11-04 20:03:59 +0000

    Created Input components and styles for inputs + fix parsing in MyButton component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
+import MyButton from './components/button/MyButton'
+import MyInput from './components/input/MyInput'
 import RecipeList from './components/RecipeList'
 import './styles/App.css'
-
 
 
 function App() {
@@ -12,9 +13,20 @@ function App() {
     { id: 3, title: 'Lentil soup', body: 'Spicy and rich soup for cold days' },
   ])
 
+  const [recipes2, setRecipe2] = useState([
+    { id: 1, title: 'Plov with meat', body: 'Hearty soup for cold days' },
+    { id: 2, title: 'Plov with curaga', body: 'Perfect for autumn chilly days' },
+    { id: 3, title: 'Plov with mushrooms', body: 'Spicy and rich soup for cold days' },
+  ])
+
   return (
     <div className="App">
-    <RecipeList recipes={recipes}/>
+      <MyInput type="text" placeholder='Title' />
+      <MyInput type="text" placeholder='Description' />
+      <MyButton disabled children>Add recipe</MyButton>
+
+    <RecipeList recipes={recipes} title='Рецепты супов'/>
+    <RecipeList recipes={recipes2} title='Рецепты плова'/>
     </div>
   )
 }
