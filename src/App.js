7b799218f3c5195b52475fe8d67645c5a67b2555
AnnaCodes1 <annacodes1@gmail.com>
2022-11-04 11:54:04 +0000

    First recipe ant it's styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './styles/App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="recipe">
+        <div className="recipe_content">
+          <strong>1. First recipe</strong>
+          <div>First recipe according to diet N5</div>
+        </div>
+        <div className="recipe__btns">
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
